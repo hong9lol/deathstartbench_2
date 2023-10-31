7a55e21c8d9304a11ac41243825543ea41e158ba
leejaehong <ljh890322@gmail.com>
2023-11-01 08:22:51 +0900

    use ClusterIP

--- init_env.sh
+++ init_env.sh
@@ -22,15 +22,25 @@ helm install social-network ./socialNetwork/helm-chart/socialnetwork/
 sleep 120
 
 # ---------------------------------------- #
-echo 4. Enable nginx-server
-minikube service nginx-thrift
-minikube service list | grep nginx-thrift |  awk '/[[:space:]]/ {print $8}' > target_url.txt
+echo 4. Expose nginx-server and jaeger
+# minikube service nginx-thrift
+# minikube service list | grep nginx-thrift |  awk '/[[:space:]]/ {print $8}' > target_url.txt
+# minikube service jaeger
+# minikube service list | grep jaeger |  awk '/[[:space:]]/ {print $8}' > jaeger_url.txt
+kubectl port-forward svc/nginx-thrift 8080:8080
+kubectl port-forward svc/jaeger 5775:5775
+kubectl port-forward svc/jaeger 6831:6831
+kubectl port-forward svc/jaeger 6832:6832
+kubectl port-forward svc/jaeger 5778:5778
+kubectl port-forward svc/jaeger 16686:16686
+kubectl port-forward svc/jaeger 14268:14268
+kubectl port-forward svc/jaeger 9411:9411
 
 # ---------------------------------------- #
 echo 5. Start Metrics-server
 kubectl delete -n kube-system deployments.apps metrics-server
 minikube addons enable metrics-server
-
+sleep 120
 
 # ---------------------------------------- #
 echo 6. Run traffic generlator
@@ -39,24 +49,4 @@ url=$(echo $raw_url | tr "\"" "\n")
 # ... $url
 
 
-# echo 2. Start Simple Application
-# kubectl apply -f k8s/manifests/simple_app.yaml
-# kubectl expose deployment simple-app-deployment --type=LoadBalancer --port=8080 & 
-
-# minikube service simple-app-deployment
-# minikube service list -n default -o json | jq '.[1].URLs[0]' > target_url.txt
-
-# echo 3. Start Metrics-server
-# kubectl delete -n kube-system deployments.apps metrics-server
-# minikube addons enable metrics-server
-# sleep 120
-
-# image=`kubectl get deployments.apps -n kube-system metrics-server -o wide --no-headers | awk '/[[:space:]]/ {print $7}'`
-# echo $image
-# kubectl patch -n kube-system deployments.apps metrics-server -p '{"spec":{"template":{"spec":{"containers":[{"name":"metrics-server", "image": "'${image}'", "args":["--cert-dir=/tmp","--secure-port=4443","--kubelet-preferred-address-types=InternalIP,ExternalIP,Hostname","--kubelet-use-node-status-port","--metric-resolution=15s","--kubelet-insecure-tls"]}]}}}}'
-# sleep 10
-# kubectl rollout restart -n kube-system deployments.apps metrics-server
-# kubectl get deployments.apps -n kube-system metrics-server --template='{{range $k := .spec.template.spec.containers}}{{$k.args}}{{"\n"}}{{end}}' | grep -o 'metric-resolution=[^ ]*'
-
 echo ====== Done ======
-#python3 benchmark/utils/kubelet_interval_test.py
