3fddf349f8f655e9a57910b9511ff684c1de1848
hong9lol <ljh890322@gmail.com>
2023-11-01 14:05:31 +0900

    update init
    Signed-off-by: hong9lol <ljh890322@gmail.com>

--- init_env.sh
+++ init_env.sh
@@ -17,17 +17,24 @@ make
 cd ..
 
 # ---------------------------------------- #
-echo 3. Install helm package
+echo 3k . Install helm package
 helm install social-network ./socialNetwork/helm-chart/socialnetwork/
 sleep 120
 
 # ---------------------------------------- #
-echo 4. Expose nginx-server and jaeger
+echo 4. Start Metrics-server
+kubectl delete -n kube-system deployments.apps metrics-server
+minikube addons enable metrics-server
+sleep 120
+
+# ---------------------------------------- #
+echo 5. Expose nginx-server, media-front and jaeger
 # minikube service nginx-thrift
 # minikube service list | grep nginx-thrift |  awk '/[[:space:]]/ {print $8}' > target_url.txt
 # minikube service jaeger
 # minikube service list | grep jaeger |  awk '/[[:space:]]/ {print $8}' > jaeger_url.txt
 kubectl port-forward svc/nginx-thrift 8080:8080 &
+kubectl port-froward svc/media-frontend 8081:8081 &
 kubectl port-forward svc/jaeger 5775:5775 &
 kubectl port-forward svc/jaeger 6831:6831 &
 kubectl port-forward svc/jaeger 6832:6832 &
@@ -35,12 +42,6 @@ kubectl port-forward svc/jaeger 5778:5778 &
 kubectl port-forward svc/jaeger 16686:16686 &
 kubectl port-forward svc/jaeger 14268:14268 &
 kubectl port-forward svc/jaeger 9411:9411 &
-
-# ---------------------------------------- #
-echo 5. Start Metrics-server
-kubectl delete -n kube-system deployments.apps metrics-server
-minikube addons enable metrics-server
-sleep 120
 
 # ---------------------------------------- #
 echo 6. Run traffic generlator
--- init_env_mac.sh
+++ init_env_mac.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 echo ====== Start ======
+brew install luarocks
+brew install libsolv
+brew install libdv
 
 # ---------------------------------------- #
 echo 1. Init Minikube
@@ -18,12 +21,19 @@ helm install social-network ./socialNetwork/helm-chart/socialnetwork/
 sleep 120
 
 # ---------------------------------------- #
-echo 4. Expose nginx-server and jaeger
+echo 4. Start Metrics-server
+kubectl delete -n kube-system deployments.apps metrics-server
+minikube addons enable metrics-server
+sleep 120
+
+# ---------------------------------------- #
+echo 5. Expose nginx-server, media-front and jaeger
 # minikube service nginx-thrift
 # minikube service list | grep nginx-thrift |  awk '/[[:space:]]/ {print $8}' > target_url.txt
 # minikube service jaeger
 # minikube service list | grep jaeger |  awk '/[[:space:]]/ {print $8}' > jaeger_url.txt
 kubectl port-forward svc/nginx-thrift 8080:8080 &
+kubectl port-forward svc/media-frontend 8081:8081 &
 kubectl port-forward svc/jaeger 5775:5775 &
 kubectl port-forward svc/jaeger 6831:6831 &
 kubectl port-forward svc/jaeger 6832:6832 &
@@ -31,12 +41,6 @@ kubectl port-forward svc/jaeger 5778:5778 &
 kubectl port-forward svc/jaeger 16686:16686 &
 kubectl port-forward svc/jaeger 14268:14268 &
 kubectl port-forward svc/jaeger 9411:9411 &
-
-# ---------------------------------------- #
-echo 5. Start Metrics-server
-kubectl delete -n kube-system deployments.apps metrics-server
-minikube addons enable metrics-server
-sleep 120
 
 # ---------------------------------------- #
 echo 6. Run traffic generlator
